6422a452a3f0d5399a7a9ec13c1713238b20e83f
Hung Truong Van <xtruongvanhung@gmail.com>
2026-08-08 18:45:21 +0700

    Add commit 3

--- src/test/java/com/mycompany/w11_ktpm_unittestwithmaven/SeleniumTest.java
+++ src/test/java/com/mycompany/w11_ktpm_unittestwithmaven/SeleniumTest.java
@@ -23,7 +23,7 @@ public class SeleniumTest {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--headless=new");
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
